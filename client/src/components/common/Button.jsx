de290337d9037e4d7d348cc01a3388ba7b4d6b42
Vitali Griunvald <vgriunvald@gmail.com>
2023-09-23 14:43:22 +0300

    Change button padding and background color

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -1,8 +1,8 @@
 function Button({ name, type }) {
   return (
-    <button className={`px-3 py-2 text-lg font-semibold rounded 
-        ${type === 'primary' ? 'bg-green-600 text-white' :
-                'bg-inherit text-black border-black border-solid border-2'}`}>
+    <button className={`px-8 py-2 text-lg font-semibold rounded 
+        ${type === 'primary' ? 'bg-black text-white' :
+                'bg-inherit text-black'}`}>
       {name}
     </button>
   );
