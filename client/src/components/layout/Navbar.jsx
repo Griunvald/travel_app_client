529858eb8d80bffd56ba148d24c7d71baf25dc74
Vitali Griunvald <vgriunvald@gmail.com>
2023-09-25 10:40:59 +0300

    Conditionaly render Hamburger component and login/join buttons

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,8 +1,23 @@
+import { useState, useEffect } from 'react';
 import Button from  '../common/Button.jsx';
 import Logo from '../common/Logo.jsx';
 import Hamburger from '../common/Hamburger.jsx';
 
 function Navbar() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <nav className="bg-gray-200 flex justify-between">
             <div className="flex justify-start items-center ml-4.5">
@@ -10,9 +25,14 @@ function Navbar() {
             </div>
             <div className="max-w-full bg-gray-200">
                 <div className="flex justify-end gap-x-5 my-4 mr-4.5">
+        { windowWidth > 768 ? (
+            <>
                 <Button name="Login" type="secondary"/>
                 <Button name="Join" type="primary"/>
-        <Hamburger />
+            </>
+        ) : (
+                <Hamburger />
+        )}
                 </div>
             </div>
         </nav>
