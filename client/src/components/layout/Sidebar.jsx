9d8ea81e6bda35c49c7c082336c110d8253cbac3
Vitali Griunvald <vgriunvald@gmail.com>
2023-09-26 09:16:03 +0300

    Change sidebar background color, import Log in and Join buttons

--- client/src/components/layout/Sidebar.jsx
+++ client/src/components/layout/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { useMenu } from '../../contexts/MenuContext';
+import Button from  '../common/Button.jsx';
 
 const Sidebar = () => {
   const { isOpen, toggleMenu } = useMenu();
@@ -6,11 +7,17 @@ const Sidebar = () => {
   return (
     <>
       <div onClick={toggleMenu} 
-           className={`fixed inset-0 bg-black opacity-50 ${isOpen ? 'block' : 'hidden'} z-40`} 
+           className={`fixed inset-0 bg-black opacity-50 
+           ${isOpen ? 'block' : 'hidden'} z-40`} 
       />
-      <div className={`fixed top-0 right-0 h-full w-[80%] bg-gray-900 transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-full'} z-50`}>
-        {/* Sidebar content here */}
-        <p className="text-white p-8">Hello, I'm your sidebar!</p>
+      <div className={`fixed top-0 right-0 h-full w-[80%]
+          bg-gray-200 transform transition-transform duration-300
+          ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-full'} z-50`}>
+      <div className="flex flex-col mx-4.5 gap-y-16 mt-16">
+           <Button type="secondary" name="Log in" />
+           <Button type="primary" name="Join" />
+      </div>
+        
       </div>
     </>
   );
