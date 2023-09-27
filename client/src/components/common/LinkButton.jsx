0a792e3aac973f6cea37a75df8445f58a69f2708
Vitali Griunvald <vgriunvald@gmail.com>
2023-09-27 13:26:39 +0300

    Add onClick prop to a LinkButton - toggle sidebar on button click

--- client/src/components/common/LinkButton.jsx
+++ client/src/components/common/LinkButton.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
 
-function LinkButton({ name, type, path }) {
+function LinkButton({ name, type, path, onClick }) {
   return (
         <Link to={path} className={`px-8 py-2 text-lg font-semibold rounded text-center 
             ${type === 'primary' ? 'bg-black text-white' :
-                    'bg-inherit text-black'}`}>
+                    'bg-inherit text-black'}`} onClick={onClick}>
           {name}
         </Link>
   );
