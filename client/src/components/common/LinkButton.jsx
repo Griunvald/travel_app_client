e95d02420f1f51133761683ab6e247ee4415f4eb
Vitali Griunvald <vgriunvald@gmail.com>
2023-09-27 12:26:50 +0300

    Align text center at LinkButton

--- client/src/components/common/LinkButton.jsx
+++ client/src/components/common/LinkButton.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function LinkButton({ name, type, path }) {
   return (
-        <Link to={path} className={`px-8 py-2 text-lg font-semibold rounded 
+        <Link to={path} className={`px-8 py-2 text-lg font-semibold rounded text-center 
             ${type === 'primary' ? 'bg-black text-white' :
                     'bg-inherit text-black'}`}>
           {name}
