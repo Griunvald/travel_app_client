29c85ad6f740203f355bacb02475808f2ccf7419
Vitali Griunvald <vgriunvald@gmail.com>
2023-09-28 10:20:22 +0300

    Refactoring - change NotFound page content, add styles

--- client/src/components/pages/NotFound.jsx
+++ client/src/components/pages/NotFound.jsx
@@ -1,13 +1,10 @@
 import LinkButton from '../common/LinkButton';
 function NotFound(){
     return(
-        <>
-            <h2>Stranded in the Digital Wilderness?</h2>  
-            <h3>No GPS signal found!</h3>
-            <p>Why not navigate back to familiar territory? Your next adventure is just a click away.</p>
-
-            <LinkButton name="Teleport Home" type="primary" path="/"/>
-        </>    
+        <div className="flex flex-col justify-center items-center h-screen">
+            <h2 className="font-montserrat font-bold text-5xl leading-loose text-center mb-8">It seems you're looking for a route that doesn't exist</h2>  
+            <LinkButton className="mx-auto block" name="Take Me Home" type="primary" path="/"/>
+        </div>    
     ) 
 
 }
