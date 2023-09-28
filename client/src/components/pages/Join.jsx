ec31cca1f6f3a92fd432364b0953658482449936
Vitali Griunvald <vgriunvald@gmail.com>
2023-09-28 18:19:10 +0300

    Add markup and syles to Join component

--- client/src/components/pages/Join.jsx
+++ client/src/components/pages/Join.jsx
@@ -1,6 +1,24 @@
+import Button from '../common/Button';
+import  Link  from '../common/Link'
+
 function Join() {
     return (
-        <h1>Join page</h1>
+        <div className="w-full md:w-[350px] mx-auto border-solid border-black border-x border-y px-12 pt-6 pb-6 mt-24">
+        <h2 className="font-playfair-display font-bold text-2xl text-center mb-6">Join Road Cronicles</h2>
+          <form className="flex flex-col">
+
+        <label htmlFor="username">Username</label>
+        <input className="mb-4" type="text" name="username" />
+
+        <label htmlFor="email">Email</label>
+        <input className="mb-4" type="email" name="email" />
+
+        <label htmlFor="password">Password</label>
+        <input className="mb-5" type="password" name="password" />
+        <Button name="join" type="primary"/>
+          </form>
+        <p className="mt-4">Already have an account? <Link name="Log in" path="/login"/></p>
+        </div>
     );
 }
 
