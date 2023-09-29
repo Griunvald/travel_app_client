a097bf414114a180b66c6ef9ac36c6bb41b27f21
Vitali Griunvald <vgriunvald@gmail.com>
2023-09-29 12:09:51 +0300

    Add markup and styles to Log in component

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,7 +1,23 @@
+import Button from '../common/Button';
+import  Link  from '../common/Link'
+
 function Login() {
     return (
-        <h1>Log in page</h1>
+        <div className="w-full md:w-[350px] mx-auto border-solid border-black border-x border-y px-12 pt-6 pb-6 mt-24">
+        <h2 className="font-playfair-display font-bold text-2xl text-center mb-6">Log In to Road Cronicles</h2>
+          <form className="flex flex-col">
+
+        <label htmlFor="usernameOrEmail">Username or email</label>
+        <input className="mb-4" type="text" name="usernameOrEmail" />
+
+        <label htmlFor="password">Password</label>
+        <input className="mb-5" type="password" name="password" />
+        <Button name="Log in" type="primary"/>
+          </form>
+        <p className="mt-4">Don't have an account? <Link name="Join" path="/join"/></p>
+        </div>
     );
 }
 
 export default Login;
+
