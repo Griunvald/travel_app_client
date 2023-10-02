db4b3a4602f520054d03d7c350274ac0217b9df9
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-02 13:05:31 +0300

    Connect Login component to backend, redirect to / on success login

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,18 +1,71 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../common/Button';
 import  Link  from '../common/Link'
+import { useUser } from '../../contexts/UserContext';
+
 
 function Login() {
+    const [formData, setFormData] = useState({
+        input: "",
+        password: "",
+    });
+
+    const { setUsername } = useUser();
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+        const response = await postData('http://localhost:3003/api/v1/auth/login', formData)
+            const username = JSON.parse(response);
+            if(username) {
+                setUsername(username);
+               navigate('/');
+            }
+        } catch(err){
+           console.log(err);
+        }
+    }
+
+    const postData = async (url, data) => {
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data), 
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+      }
+};
+
     return (
         <div className="w-full md:w-[350px] mx-auto border-solid border-black border-x border-y px-12 pt-6 pb-6 mt-24">
         <h2 className="font-playfair-display font-bold text-2xl text-center mb-6">Log In to Road Cronicles</h2>
-          <form className="flex flex-col">
+          <form className="flex flex-col" onSubmit={handleSubmit}>
 
         <label htmlFor="usernameOrEmail">Username or email</label>
-        <input className="mb-4" type="text" name="usernameOrEmail" />
+        <input className="mb-4" type="text" name="input" value={formData.input} onChange={handleChange} />
 
         <label htmlFor="password">Password</label>
-        <input className="mb-5" type="password" name="password" />
-        <Button name="Log in" variant="primary"/>
+        <input className="mb-5" type="password" name="password" value={formData.password} onChange={handleChange} />
+        <Button name="Log in" variant="primary" action="submit"/>
           </form>
         <p className="mt-4">Don't have an account? <Link name="Join" path="/join"/></p>
         </div>
