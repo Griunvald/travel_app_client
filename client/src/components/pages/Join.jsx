fc3c3e7984b481449038d91b1411a1064030488b
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-05 16:14:23 +0300

    Refactoring - replace Join form with Form and Input components

--- client/src/components/pages/Join.jsx
+++ client/src/components/pages/Join.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../common/Button';
 import  Link  from '../common/Link'
+import Input from '../common/Input';
+import Form from '../common/Form';
 
 function Join() {
     const [formData, setFormData] = useState({
@@ -54,22 +56,45 @@ function Join() {
     return (
         <div className="w-full md:w-[350px] mx-auto shadow-md border-x border-y px-12 pt-6 pb-6 mt-24">
         <h2 className="font-playfair-display font-bold text-2xl text-center mb-6">Join Road Cronicles</h2>
-          <form className="flex flex-col" onSubmit={handleSubmit}>
-
-        <label htmlFor="email">Email</label>
-        <input className="mb-4" type="email" name="email" value={formData.email} onChange={handleChange}/>
-        
-        <label htmlFor="fullname">Full name</label>
-        <input className="mb-4" type="text" name="fullname" value={formData.fullname} onChange={handleChange}/>
-
-        <label htmlFor="username">Username</label>
-        <input className="mb-4" type="text" name="username" value={formData.username} onChange={handleChange}/>
 
 
-        <label htmlFor="password">Password</label>
-        <input className="mb-5" type="password" name="password" value={formData.password} onChange={handleChange}  />
-        <Button name="join" variant="primary" action="submit"/>
-          </form>
+            <Form onSubmit={handleSubmit}>
+
+                <Input
+                    label="Email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                
+            />
+
+                <Input
+                    label="Full name"
+                    name="fullname"
+                    value={formData.fullname}
+                    onChange={handleChange}
+            />
+
+                <Input
+                    label="Username"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+            />
+
+                <Input
+                    label="Password"
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                
+            />
+
+            <Button name="Join" variant="primary" action="submit"/>
+
+            </Form>
+
         <p className="mt-4">Already have an account? <Link name="Log in" path="/login"/></p>
         </div>
     );
