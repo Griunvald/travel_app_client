49bed4d6ebcf72a3479dcc6089af338883d43bd3
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-09 00:28:07 +0300

    Change Button component colors

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -1,8 +1,8 @@
 function Button({ name, variant, action }) {
   return (
-    <button className={`px-8 py-2 text-lg font-semibold rounded 
-        ${variant === 'primary' ? 'bg-black text-white' :
-                'bg-inherit text-black'}`} type={action}>
+    <button className={`px-8 py-2 text-base font-medium rounded 
+        ${variant === 'primary' ? 'bg-accent text-white' :
+                'bg-inherit text-gray-900'}`} type={action}>
       {name}
     </button>
   );
