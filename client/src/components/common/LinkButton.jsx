7b87b10b48465f927e7d1db5136f687cf2c9c69b
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-08 23:24:14 +0300

    Change LinkButton styles

--- client/src/components/common/LinkButton.jsx
+++ client/src/components/common/LinkButton.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 function LinkButton({ name, variant, path, onClick }) {
   return (
-        <Link to={path} className={`px-8 py-2 text-lg font-semibold rounded text-center 
-            ${variant === 'primary' ? 'bg-black text-white' :
-                    'bg-inherit text-black'}`} onClick={onClick}>
+        <Link to={path} className={`px-8 py-2 text-base font-medium rounded text-center 
+            ${variant === 'primary' ? 'bg-accent text-white' :
+                    'bg-inherit text-gray-900 border border-primary'}`} onClick={onClick}>
           {name}
         </Link>
   );
