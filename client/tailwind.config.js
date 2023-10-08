91264d1ff1701740725b4e855f4715da8602841f
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-08 23:03:12 +0300

    Add custom border color, baclground color and shadows to tailwind.config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -17,6 +17,21 @@ export default {
             'gray-600': '#BDBDBD',
             'gray-500': '#E0E0E0',
           },
+           borderColor: {
+            'primary': '#333333', 
+            'secondary': '#828282',
+            'tertiary': '#BDBDBD',
+      },
+          backgroundColor: {
+            'primary': '#ffffff',
+            'secondary': '#f2f2f2',
+            'accent': '#333333',
+      },
+          boxShadow: {
+            'soft': '0 4px 6px rgba(0, 0, 0, 0.1)',
+            'medium': '0 6px 15px rgba(0, 0, 0, 0.1)',
+            'strong': '0 8px 24px rgba(0, 0, 0, 0.2)',
+      },
           spacing: {
               '4.5':'1.125rem'
           }
