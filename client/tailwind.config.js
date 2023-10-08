5573b943980c408ce6a1f9da70e93d4033280d49
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-08 17:23:02 +0300

    Add custom text colors classes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,6 +10,13 @@ export default {
               'pacifico': ['Pacifico', 'cursive'],
               'sans': ['Roboto', 'sans-serif']
           },
+          textColor: {
+            'gray-900': '#333333',
+            'gray-800': '#4F4F4F',
+            'gray-700': '#828282',
+            'gray-600': '#BDBDBD',
+            'gray-500': '#E0E0E0',
+          },
           spacing: {
               '4.5':'1.125rem'
           }
