d74079009dd3f7737414f019a9edb5f99b076816
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-08 15:36:45 +0300

    Hide corner in text area that alow scale it using mouse

--- client/src/components/common/Textarea.jsx
+++ client/src/components/common/Textarea.jsx
@@ -3,7 +3,7 @@ function Textarea({ label, name, value, onChange, rows = 5, cols = 40 }) {
     <div className="flex flex-col">
       {label && <label htmlFor={name}>{label}</label>}
       <textarea
-        className="mb-4"
+        className="mb-4 resize-none overflow-hidden"
         id={name}
         name={name}
         rows={rows}
