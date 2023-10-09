8dab2b899818b0052be7cef6461e4b98ae060eb4
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-09 14:38:05 +0300

    Add fixed height to LinkButton

--- client/src/components/common/LinkButton.jsx
+++ client/src/components/common/LinkButton.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function LinkButton({ name, variant, path, onClick }) {
   return (
-        <Link to={path} className={`px-8 py-2 text-base font-medium rounded text-center 
+        <Link to={path} className={`h-10 px-8 py-2 text-base font-medium rounded text-center 
             ${variant === 'primary' ? 'bg-accent text-white' :
                     'bg-inherit text-gray-900 border border-primary'}`} onClick={onClick}>
           {name}
