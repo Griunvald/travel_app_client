97977a4fa85984f588938246d8f8d114f4cc507a
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-09 16:43:31 +0300

    Make Button and ButtonLink components full width only on desktop

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -1,6 +1,6 @@
 function Button({ name, variant, action }) {
   return (
-    <button className={`px-8 py-2 text-base font-medium rounded 
+    <button className={`md:max-w-max px-8 py-2 text-base font-medium rounded 
         ${variant === 'primary' ? 'bg-accent text-white' :
                 'bg-inherit text-gray-900'}`} type={action}>
       {name}
