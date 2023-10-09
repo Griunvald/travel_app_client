4014a1d0c63a7ed81da02601335fad4c86117e54
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-09 15:45:27 +0300

    Change styles in Textarea component

--- client/src/components/common/Textarea.jsx
+++ client/src/components/common/Textarea.jsx
@@ -1,7 +1,7 @@
 function Textarea({ label, name, value, onChange, rows = 5, cols = 40 }) {
   return (
     <div className="flex flex-col">
-      {label && <label htmlFor={name}>{label}</label>}
+      {label && <label className="text-gray-800" htmlFor={name}>{label}</label>}
       <textarea
         className="mb-4 resize-none overflow-hidden"
         id={name}
