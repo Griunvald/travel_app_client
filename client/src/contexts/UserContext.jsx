b13df54286dd2306c44a9086e07f6d835f7cde81
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-11 15:09:37 +0300

    Update userContext - add userId property

--- client/src/contexts/UserContext.jsx
+++ client/src/contexts/UserContext.jsx
@@ -9,17 +9,19 @@ export function useUser() {
 
 export function UserProvider({ children }) {
   const [username, setUsername] = useState('');
+  const [userId, setUserId] = useState(null);
 
   useEffect(() => {
     const userInfo = getCookie('user_info'); 
     if (userInfo) {
       const parsedInfo = JSON.parse(userInfo);
       setUsername(parsedInfo.username);
+      setUserId(parsedInfo.userId);
     }
   }, []);
 
   return (
-    <UserContext.Provider value={{ username, setUsername }}>
+    <UserContext.Provider value={{ username, setUsername, userId, setUserId}}>
       {children}
     </UserContext.Provider>
   );
