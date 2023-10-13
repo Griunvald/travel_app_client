eb0308f2ef8234b281e98f978c7d366cfd7507f3
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-13 12:03:51 +0300

    Connect create trip component to backend, make put request to AWS. User id is undefined.

--- client/src/components/pages/StartTrip.jsx
+++ client/src/components/pages/StartTrip.jsx
@@ -2,100 +2,153 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../common/Button';
 import LinkButton from '../common/LinkButton';
-import  Link  from '../common/Link'
 import Textarea from '../common/Textarea';
 import Form from '../common/Form';
 import ImageUpload from '../common/ImageUpload';
 import { useUser } from '../../contexts/UserContext';
 import { useImage } from '../../contexts/ImageContext';
 
-
 function StartTrip() {
-    const [formData, setFormData] = useState({
-        title: "",
-        description: "",
-    });
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
 
-    const { setUsername } = useUser();
-    const { handleCancel, preview, imageFile } = useImage();
-    const navigate = useNavigate();
+  const { userId } = useUser();
+   console.log("User id from app state: ", userId);
+  const { handleCancel, preview, imageFile } = useImage();
+  const navigate = useNavigate();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleSubmit = async (e) => {
-        try {
-        const response = await postData('http://localhost:3003/api/v1/auth/login', formData)
-            const username = JSON.parse(response);
-            if(username) {
-                setUsername(username);
-               navigate('/');
-            }
-        } catch(err){
-           console.log(err);
-        }
+const handleSubmit = async (e) => {
+    let awsData = {};
+    console.log("User ID:", userId);
+
+    try {
+        awsData = await getData('http://localhost:3003/api/v1/file/get-signed-url', userId);
+        let { presignedAwsUrl, awsObjectKey } = awsData;
+
+        console.log("Presigned AWS URL:", presignedAwsUrl);
+        console.log("AWS Object Key:", awsObjectKey);
+
+        awsObjectKey = awsObjectKey.replace('undefined', userId);
+
+        await putData(presignedAwsUrl, imageFile);
+
+        const newData = {
+            userId,
+            ...formData,
+            url: awsObjectKey 
+        };
+
+        const responseFromPost = await postData('http://localhost:3003/api/v1/trip/create-trip', newData);
+        console.log("Response from POST:", responseFromPost);
+        
+    } catch (err) {
+        console.error("Error in handleSubmit:", err);
     }
-
-    const postData = async (url, data) => {
-      try {
-        const response = await fetch(url, {
-          method: 'POST',
-          credentials: 'include',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data), 
-        });
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const result = await response.json();
-        return result;
-      } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-      }
 };
 
-    return (
-        <div className="w-full md:w-[600px] mx-auto md:border md:border-primary md:shadow-soft px-2 md:px-12 pt-6 pb-6 mt-6 md:mt-24">
-            <h2 className="font-medium text-2xl text-center mb-6">Ready for a New Journey? Let’s Get Started!</h2>
 
-            <Form onSubmit={handleSubmit}>
 
-                <Textarea
-                    label="Trip Title"
-                    name="title"
-                    rows="1" 
-                    value={formData.title}
-                    onChange={handleChange}
-                
-            />
+  const postData = async (apiUrl, data) => {
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
 
-                <Textarea
-                    label="Trip Description"
-                    name="description"
-                    rows="3" 
-                    value={formData.description}
-                    onChange={handleChange}
-                
-            />
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-            <ImageUpload
-                label="Cover Image"
-            />
+      return await response.json();
+    } catch (error) {
+      console.error('There was a problem with the post operation:', error);
+    }
+  };
 
-<div className="flex flex-col  md:flex-row justify-end gap-x-5 gap-y-4">
-             {preview && <LinkButton name="Remove Image" type="secondary" onClick={handleCancel} />}
-            <Button name="Publish" variant="primary" action="submit"/>
+  const putData = async (presignedAwsUrl, file) => {
+    try {
+      const response = await fetch(presignedAwsUrl, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': file.type,
+        },
+        body: file,
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      return presignedAwsUrl;
+    } catch (error) {
+      console.error('There was a problem with the put operation:', error);
+    }
+  };
+
+  const getData = async (apiUrl, userId) => {
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          'user-id': userId,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+        const result = await response.json();
+
+   return {
+        presignedAwsUrl: result.signedUrl.url,
+        awsObjectKey: result.signedUrl.key
+    };
+
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  };
+
+  return (
+    <div className="w-full md:w-[600px] mx-auto md:border md:border-primary md:shadow-soft px-2 md:px-12 pt-6 pb-6 mt-6 md:mt-24">
+      <h2 className="font-medium text-2xl text-center mb-6">Ready for a New Journey? Let’s Get Started!</h2>
+      <Form onSubmit={handleSubmit}>
+        <Textarea
+          label="Trip Title"
+          name="title"
+          rows="1"
+          value={formData.title}
+          onChange={handleChange}
+        />
+        <Textarea
+          label="Trip Description"
+          name="description"
+          rows="3"
+          value={formData.description}
+          onChange={handleChange}
+        />
+        <ImageUpload label="Cover Image" />
+        <div className="flex flex-col  md:flex-row justify-end gap-x-5 gap-y-4">
+          {preview && <LinkButton name="Remove Image" type="secondary" onClick={handleCancel} />}
+          <Button name="Publish" variant="primary" action="submit" />
         </div>
-
-            </Form>
-        </div>
-    );
+      </Form>
+    </div>
+  );
 }
 
 export default StartTrip;
