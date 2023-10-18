882e4145b7b469160b69f823b672e62dd74ce5d4
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-18 14:52:30 +0300

    Add avatar to TripPreview component

--- client/src/components/common/TripPreview.jsx
+++ client/src/components/common/TripPreview.jsx
@@ -1,10 +1,15 @@
-function TripPreview({id, username,  title, description, url, createdAt}) {
+import Avatar from './Avatar.jsx';
+
+function TripPreview({id, username,  title, description, url, createdAt, avatar}) {
     return(
         <div className="w-full md:w-[800px] mx-auto pt-4 md:pt-6 mb-4 border border-2 shadow-soft p-2" key={id}>
             <h1 className="font-bold text-gray-900 text-3xl md:text-4xl">{title}</h1>
-            <div className="my-2">
-              <p className="font-semibold text-sm">{username}</p>
-              <p className="text-sm">{createdAt}</p>
+            <div className="my-2 py-2 flex gap-2">
+            <Avatar avatar={avatar} />
+            <div className="mt-2">
+                  <p className="font-semibold text-sm">{username}</p>
+                  <p className="text-sm">{createdAt}</p>
+            </div>
             </div>
             <p className="text-base font-normal">{description}</p>
             <img className="w-full md:w-[800px] md:h-[600x] object-cover mt-4" src={url} alt="trip cover image" />
