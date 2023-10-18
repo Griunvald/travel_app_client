34bf615dd17d8c79c555b83e489bacea734adbd9
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-18 16:38:54 +0300

    Decrease Avatar size on mobile

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -2,7 +2,7 @@ import avatarIcon from '../../assets/avatar.svg';
 function Avatar({ avatar }) {
     return (
         <div>
-           <img src={avatar ? avatar : avatarIcon} alt="user avatar" /> 
+           <img className="h-9 w-9 md:h-12 md:w-12" src={avatar ? avatar : avatarIcon} alt="user avatar" /> 
         </div>
     );
 };
