1cde4e81dcf66af1d8893db6ae7660178664939f
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-19 18:12:03 +0300

    Refactoring - get current trip id from server on app load, update button at Navbar

--- client/src/contexts/UserContext.jsx
+++ client/src/contexts/UserContext.jsx
@@ -20,12 +20,35 @@ export function UserProvider({ children }) {
       setUserId(parsedInfo.userId);
     }
 
-    const tripInfo = getCookie('trip_info'); 
-    if (tripInfo) {
-      const parsedInfo = JSON.parse(tripInfo);
-      setCurrentTripId(parsedInfo.tripId);
-    }
   }, []);
+
+    useEffect(() => {
+  const fetchCurrentTrip = async () => {
+    try {
+      const response = await fetch('http://localhost:3003/api/v1/trip/get-trip', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userId })  // Assuming you have userId in scope
+      });
+
+      const data = await response.json();
+
+      if (data && data.id) {
+        setCurrentTripId(data.id);  // Assuming you have setCurrentTripId in scope
+        // Set your hasCurrentTrip state or context value here
+      }
+    } catch (error) {
+      console.error('Failed to fetch current trip:', error);
+    }
+  };
+
+  if (userId) {  // Only attempt to fetch if userId is set
+    fetchCurrentTrip();
+  }
+}, [userId]);  // Re-run whenever userId changes
+
 
   return (
     <UserContext.Provider value={{ username, setUsername, userId, setUserId, currentTripId, setCurrentTripId}}>
