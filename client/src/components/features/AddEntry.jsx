cb40d711434ec7e3ecc2a5ff4a4b2a21c8662107
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-21 17:08:44 +0300

    Import components needed to compose AddEntry component - except tags

--- client/src/components/features/AddEntry.jsx
+++ client/src/components/features/AddEntry.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+import TabButton from '../common/TabButton';
+import TextArea from '../common/TextArea';
+import ImageUpload from '../common/ImageUpload';
+import TabContainer from '../common/TabContainer';
+import Tab from '../common/Tab';
+
 function AddEntry(){
-    return(
-        <div>
-           <p>Add entry</p>
-        </div>
-    )
+      const [activeTab, setActiveTab] = useState('textArea');
+
+  return (
+    <>
+      <TabButton tabName="textArea" setActiveTab={setActiveTab} activeTab={activeTab} />
+      <TabButton tabName="imageUpload" setActiveTab={setActiveTab} activeTab={activeTab} />
+
+      <TabContainer activeTab={activeTab}>
+        <Tab tabName="textArea">
+          <TextArea />
+        </Tab>
+        <Tab tabName="imageUpload">
+          <ImageUpload />
+        </Tab>
+      </TabContainer>
+    </>
+  );
 }
 
 export default AddEntry;
