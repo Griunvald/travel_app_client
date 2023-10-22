1e6b70332e03e36c77ccfbb55fbe12c560e45b8c
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-22 23:14:14 +0300

    Add TagsContext

--- client/src/contexts/ProvidersWrapper.jsx
+++ client/src/contexts/ProvidersWrapper.jsx
@@ -1,14 +1,17 @@
 import { MenuProvider } from './MenuContext.jsx';
 import { UserProvider } from './UserContext.jsx';
 import { ImageProvider } from './ImageContext.jsx';
+import { TagsProvider } from './TagsContext.jsx';
 
 export const ProvidersWrapper = ({ children }) => {
     return (
         <UserProvider>
           <MenuProvider>
-        <ImageProvider>
-            { children }
-        </ImageProvider>
+            <ImageProvider>
+               <TagsProvider>
+                { children }
+               </TagsProvider>
+            </ImageProvider>
           </MenuProvider>
         </UserProvider>
     );
