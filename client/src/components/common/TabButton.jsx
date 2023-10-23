084b0a6cfb05720538a58c9f79c2d2ba8d247469
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-23 13:22:13 +0300

    Change tab active and inactive classes

--- client/src/components/common/TabButton.jsx
+++ client/src/components/common/TabButton.jsx
@@ -1,6 +1,6 @@
 function TabButton({ tabName, setActiveTab, activeTab }) {
   const baseClasses = 'max-w-max px-8 py-2 text-base font-medium bg-inherit text-gray-900';
-  const activeClasses = activeTab === tabName ? 'underline decoration-solid' : '';
+  const activeClasses = activeTab === tabName ? 'border-b-4 border-primary' : 'text-gray-600';
 
   return (
     <button 
