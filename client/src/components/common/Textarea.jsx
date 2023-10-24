e46557cd7345061617947689fbbae75b7dc3b462
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-24 11:53:59 +0300

    Add height property to Textarea component

--- client/src/components/common/Textarea.jsx
+++ client/src/components/common/Textarea.jsx
@@ -1,14 +1,15 @@
-function Textarea({ label, name, value, onChange, rows = 5, cols = 40 }) {
+function Textarea({ label, name, value, onChange, height, rows = 5, cols = 40 }) {
   return (
     <div className="flex flex-col">
       {label && <label className="text-gray-800" htmlFor={name}>{label}</label>}
       <textarea
-        className="mb-4 resize-none overflow-hidden"
+        className={`mb-4 resize-none overflow-hidden h-${height}`}
         id={name}
         name={name}
         rows={rows}
         cols={cols}
         value={value}
+        height={height}
         onChange={onChange}
       />
     </div>
