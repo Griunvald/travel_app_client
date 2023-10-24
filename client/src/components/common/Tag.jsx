6c9c185a8531e874d49738449af81c28dbc9592c
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-24 17:45:08 +0300

    Add margin and change font size of Tag component

--- client/src/components/common/Tag.jsx
+++ client/src/components/common/Tag.jsx
@@ -1,6 +1,6 @@
 const Tag = ({ label, onRemove }) => {
   return (
-    <span className="tag border solid rounded-xl px-2.5 py-1 bg-secondary">
+    <span className="text-sm tag border solid rounded-xl px-2.5 py-1  m-1 bg-secondary">
       {label}
       <button className="ml-1" onClick={onRemove}>x</button>
     </span>
