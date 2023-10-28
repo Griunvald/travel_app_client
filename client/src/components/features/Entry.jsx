706201c96471f5b3a4cc1eb28a218b2e1a3fbac7
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-28 11:39:27 +0300

    Style entry component - basic

--- client/src/components/features/Entry.jsx
+++ client/src/components/features/Entry.jsx
@@ -1,19 +1,26 @@
+import React from 'react';
+import format from 'date-fns/format';
+
 function Entry({ id, createdAt, textValue, urlValue, recordTags }) {
+  // Format the createdAt date
+  const formattedDate = format(new Date(createdAt), "MMMM do, yyyy, hh:mm a");
+
   return (
-    <div>
-      <p>{createdAt}</p>
+    <div className="w-full md:w-[800px] mx-auto ">
+      <p className="text-sm">{formattedDate}</p> {/* Use the formatted date here */}
       {recordTags && (
-        <ul>
+        <ul className="flex flex-wrap">
           {recordTags.map((tag) => (
             <li key={tag.id}>{tag.tagName}</li>
           ))}
         </ul>
       )}
       {textValue && (
-        <p>{textValue}</p>
+        <p className="text-base font-normal">{textValue}</p>
       )}
       {urlValue && (
-        <img src={urlValue} alt="" />
+        <img  className="w-full md:w-[800px] md:h-[600px] object-cover mt-4" 
+          src={`https://travel-app-dev.s3.il-central-1.amazonaws.com/${urlValue}`} alt="" />
       )}
     </div>
   );
