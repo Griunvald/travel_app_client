c5e976cd198f96f014b843d17d6695207746574b
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-28 12:11:04 +0300

    Add more styles to Entry component

--- client/src/components/features/Entry.jsx
+++ client/src/components/features/Entry.jsx
@@ -6,20 +6,22 @@ function Entry({ id, createdAt, textValue, urlValue, recordTags }) {
   const formattedDate = format(new Date(createdAt), "MMMM do, yyyy, hh:mm a");
 
   return (
-    <div className="w-full md:w-[800px] mx-auto ">
-      <p className="text-sm">{formattedDate}</p> {/* Use the formatted date here */}
+    <div className="w-full md:w-[800px] mx-auto">
+      <div className="bg-secondary p-2">
+      <p className="text-sm font-semibold mb-1 mt-2">{formattedDate}</p> {/* Use the formatted date here */}
       {recordTags && (
-        <ul className="flex flex-wrap">
+        <ul className="flex gap-x-2 gap-y-3 flex-wrap mb-1 text-sm">
           {recordTags.map((tag) => (
             <li key={tag.id}>{tag.tagName}</li>
           ))}
         </ul>
       )}
+      </div>
       {textValue && (
-        <p className="text-base font-normal">{textValue}</p>
+        <p className="text-base font-normal my-4 ">{textValue}</p>
       )}
       {urlValue && (
-        <img  className="w-full md:w-[800px] md:h-[600px] object-cover mt-4" 
+        <img  className="w-full md:w-[800px] md:h-[600px] object-cover my-4" 
           src={`https://travel-app-dev.s3.il-central-1.amazonaws.com/${urlValue}`} alt="" />
       )}
     </div>
