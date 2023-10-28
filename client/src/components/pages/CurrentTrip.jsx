0fd73d0877902bbb66106e1eb39fcfafa813d417
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-28 15:07:04 +0300

    Show different date format in TripPreview component at /current-trip route

--- client/src/components/pages/CurrentTrip.jsx
+++ client/src/components/pages/CurrentTrip.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import format from 'date-fns/format';
 import { timeAgo } from '../../utils/date.js';
 import { useUser } from '../../contexts/UserContext';
 import TripPreview from '../features/TripPreview';
@@ -8,6 +9,8 @@ import EntryList from '../features/EntryList';
 function CurrentTrip() {
     const { username, userId } = useUser();
     const [trip, setTrip] = useState('');
+    const formattedDate = trip ? format(new Date(trip.createdAt), "MMMM do, yyyy, hh:mm a") : '';
+
     useEffect(() => {
         const fetchData = async () => {
             if (!userId){
@@ -26,6 +29,8 @@ function CurrentTrip() {
             const data = await response.json();
 
             setTrip(data);
+            console.log("data: ", data);
+            console.log("trip: ", trip);
            } catch (error) {
             console.error('Failed to fetch trips:', error);
       }
@@ -37,7 +42,7 @@ function CurrentTrip() {
         <TripPreview 
             key={trip.id}
             username={username}
-            createdAt={timeAgo(trip.createdAt)}
+            createdAt={formattedDate}
             title={trip.title}
             description={trip.description}
             url={`https://travel-app-dev.s3.il-central-1.amazonaws.com/${trip.url}`}
