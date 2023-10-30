77c8a228757466eac4eb6c912e81a7f626334114
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-30 14:59:49 +0200

    Add Toast component and context

---
+++ client/src/contexts/ToastContext.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const ToastContext = createContext();
+
+export const ToastProvider = ({ children }) => {
+  const [toasts, setToasts] = useState([]);
+
+  const addToast = (message, type = 'info') => {
+    const id = new Date().getTime();
+    setToasts([...toasts, { id, message, type }]);
+  };
+
+  const removeToast = (id) => {
+    setToasts(toasts.filter((toast) => toast.id !== id));
+  };
+
+  return (
+    <ToastContext.Provider value={{ toasts, addToast, removeToast }}>
+      {children}
+    </ToastContext.Provider>
+  );
+};
+
+export const useToast = () => {
+  const context = useContext(ToastContext);
+  if (!context) {
+    throw new Error('useToast must be used within a ToastProvider');
+  }
+  return context;
+};
+
