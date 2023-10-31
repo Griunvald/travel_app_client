d4e82bcbacb4d1039d337510de41cbf4a7c8395d
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-31 16:47:54 +0200

    Render data in FullTrip - except username

--- client/src/components/pages/FullTrip.jsx
+++ client/src/components/pages/FullTrip.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react';
-import format from 'date-fns/format';
 import { useParams } from 'react-router-dom';
-import { timeAgo } from '../../utils/date.js';
+import format from 'date-fns/format';
 import TripPreview from '../features/TripPreview';
-import AddEntry from '../features/AddEntry';
-import EntryList from '../features/EntryList';
+import Entry from '../features/Entry';
 
 function FullTrip() {
+    const [tripDetails, setTripDetails ] = useState({});
+    const [ entryList, setEntryList ] = useState([]);
     const { userId } = useParams();
+    console.log(tripDetails);
 
+    
     useEffect(() => {
         const fetchData = async () => {
             if (!userId){
@@ -18,10 +20,10 @@ function FullTrip() {
         try {
             const response = await fetch(`http://localhost:3003/api/v1/trip/get-full-trip?userId=${userId}`);
             const data = await response.json();
-            const { records, tripDetails } = await data;
 
-            console.log(records.rows);
-            console.log(tripDetails);
+            setTripDetails(data.tripDetails)
+            setEntryList(data.records.rows);
+
            } catch (error) {
             console.error('Failed to fetch trips:', error);
       }
@@ -30,7 +32,33 @@ function FullTrip() {
     },[userId])
     return (
       <>
-        Full trip
+        { tripDetails &&
+        <TripPreview
+            username={tripDetails.username}
+            createdAt={tripDetails.createdAt}
+          title={tripDetails.title}
+          description={tripDetails.description}
+          url={`https://travel-app-dev.s3.il-central-1.amazonaws.com/${tripDetails.url}`}
+        />
+
+        }
+        {
+
+            entryList && (
+                Array.isArray(entryList) && entryList.map(entry => (
+                <div key={entry.id}>
+                    <Entry  
+                      createdAt={entry.createdAt}
+                      textValue={entry.textValue}
+                      urlValue={entry.urlValue}
+                      recordTags={entry.recordTags}
+                    />
+                </div>
+
+                ))
+            )
+        }
+
     </>
     );
 };
