1bfdd12befe1e0e9e926f3436b1e0206a4a87fbd
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-31 09:39:57 +0200

    Refactoring - add disabled and InProgressText props to a Button component

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -1,9 +1,13 @@
-function Button({ name, variant, action }) {
+function Button({ name, variant, action, disabled, inProgressText }) {
   return (
-    <button className={`md:max-w-max px-8 py-2 text-base font-medium rounded 
+    <button 
+      className={`md:max-w-max px-8 py-2 text-base font-medium rounded 
         ${variant === 'primary' ? 'bg-accent text-white' :
-                'bg-inherit text-gray-900'}`} type={action}>
-      {name}
+                'bg-inherit text-gray-900'}`} 
+      type={action} 
+      disabled={disabled}
+    >
+      {disabled ? inProgressText : name}
     </button>
   );
 }
