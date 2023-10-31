9c5b65ed7b36c064e98eee6e55dd4776e18fc9b3
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-31 13:47:50 +0200

    Get and console.log trip details and records (entries) in FullTrip page

--- client/src/components/pages/FullTrip.jsx
+++ client/src/components/pages/FullTrip.jsx
@@ -1,6 +1,39 @@
-function FullTrip({ userId }) {
+import { useState, useEffect } from 'react';
+import format from 'date-fns/format';
+import { timeAgo } from '../../utils/date.js';
+import TripPreview from '../features/TripPreview';
+import AddEntry from '../features/AddEntry';
+import EntryList from '../features/EntryList';
+
+function FullTrip() {
+    const userId = 1;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!userId){
+                return;
+            }
+
+        try {
+            const response = await fetch(`http://localhost:3003/api/v1/trip/get-full-trip?userId=${userId}`);
+            const data = await response.json();
+            const { records, tripDetails } = await data;
+
+            console.log(records.rows);
+            console.log(tripDetails);
+           } catch (error) {
+            console.error('Failed to fetch trips:', error);
+      }
+        }
+        fetchData();
+    },[userId])
     return (
-        <div>Full trip page</div>
+      <>
+        Full trip
+    </>
     );
-}
- export default FullTrip;
+};
+
+export default FullTrip;
+
+
