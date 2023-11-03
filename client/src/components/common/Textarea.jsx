9968134fa7d729b5c303d8e0e51c78994f80f551
Vitali Griunvald <vgriunvald@gmail.com>
2023-11-03 18:20:42 +0200

    Add character count to Textarea component

--- client/src/components/common/Textarea.jsx
+++ client/src/components/common/Textarea.jsx
@@ -1,17 +1,33 @@
-function Textarea({ label, name, value, onChange, height, rows = 5, cols = 40 }) {
+function Textarea({
+  label,
+  name,
+  value,
+  onChange,
+  height,
+  rows = 5,
+  maxLength,
+}) {
   return (
     <div className="flex flex-col">
-      {label && <label className="text-gray-800" htmlFor={name}>{label}</label>}
-      <textarea
-        className={`mb-4 resize-none overflow-hidden h-${height}`}
-        id={name}
-        name={name}
-        rows={rows}
-        cols={cols}
-        value={value}
-        height={height}
-        onChange={onChange}
-      />
+      {label && (
+        <label htmlFor={name} className="text-gray-800">{label}</label>
+      )}
+      <div className="relative">
+        <textarea
+          id={name}
+          name={name}
+          rows={rows}
+          value={value}
+          onChange={onChange}
+          maxLength={maxLength}
+          className={`resize-none overflow-hidden bg-white shadow-sm mt-1 block w-full mb-4  h-${height}`}
+        />
+        {maxLength && (
+          <span className="absolute bottom-2 right-2 pb-5 text-sm text-gray-600">
+            {value.length}/{maxLength}
+          </span>
+        )}
+      </div>
     </div>
   );
 }
