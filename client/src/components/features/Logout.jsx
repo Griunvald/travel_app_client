8455ec6a80726dbbde97eb23e77e34c016a98c0d
Vitali Griunvald <vgriunvald@gmail.com>
2023-11-11 15:56:15 +0200

    Show toast notification on logout

--- client/src/components/features/Logout.jsx
+++ client/src/components/features/Logout.jsx
@@ -1,10 +1,12 @@
-import { useUser } from '../../contexts/UserContext';
 import { useNavigate } from 'react-router-dom';
+import { useUser } from '../../contexts/UserContext';
+import { useToast } from '../../contexts/ToastContext';
 import Button from '../common/Button';
 
 function Logout() {
   const url = 'http://localhost:3003/api/v1/auth/logout';
   const { setUsername, setUserId, setCurrentTripId } = useUser();
+  const { showToast } = useToast();
   const navigate = useNavigate();
 
   const handleLogout = async () => {
@@ -23,6 +25,7 @@ function Logout() {
       setUserId(null);
       setCurrentTripId(null);
       navigate('/');
+      showToast('Successfully logged out!' , {duration: 5000});
     } catch (error) {
       console.error('Error during logout:', error);
     }
