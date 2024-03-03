5123d14de70ee0b4af8b3e7ca34499721703e34f
Vitali Griunvald <me.griunvald@gmail.com>
2024-03-03 21:46:37 +0200

    Move avatar, username, date aout of Link component

--- client/src/components/features/TripPreview.jsx
+++ client/src/components/features/TripPreview.jsx
@@ -1,34 +1,37 @@
 import { Link } from 'react-router-dom';
 import Avatar from '../common/Avatar.jsx';
 
-function TripPreview({id, username,  title, description, url, createdAt, avatar, link = null}) {
+function TripPreview({ id, username, title, description, url, createdAt, avatar, link = null }) {
+    const UserInfo = (
+        <div className="flex my-2 py-2 gap-2 items-end">
+            <Avatar avatar={avatar} />
+            <div>
+                <p className="font-semibold text-sm">{username}</p>
+                <p className="text-sm">{createdAt}</p>
+            </div>
+        </div>
+    );
 
     const Content = (
         <>
             <h1 className="font-bold text-gray-900 text-3xl md:text-4xl leading-snug md:leading-normal">{title}</h1>
-            <div className="flex my-2 py-2 gap-2 items-end">
-            <Avatar avatar={avatar} />
-            <div>
-                  <p className="font-semibold text-sm">{username}</p>
-                  <p className="text-sm">{createdAt}</p>
-            </div>
-            </div>
             <p className="text-base font-normal">{description}</p>
             <img className="w-full md:w-[700px] mt-4" src={url} alt="trip cover image" />
         </>
     );
 
-    return(
-        <div className={link ? "w-full md:w-[700px] mx-auto pt-4 md:pt-6 mb-4 cursor-pointer" : "w-full md:w-[700px] mx-auto pt-4 md:pt-6 mb-4"}>
+    return (
+        <div className="w-full md:w-[700px] mx-auto pt-4 md:pt-6 mb-4">
+            {UserInfo}
             {link ? (
-                <Link to={link}>
+                <Link to={link} className="cursor-pointer">
                     {Content}
                 </Link>
             ) : (
                 Content
             )}
         </div>
-    )
+    );
 }
 
 export default TripPreview;
