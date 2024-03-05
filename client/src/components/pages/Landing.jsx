295581a106adb4a10057a0e42c9256bbf749117b
Vitali Griunvald <me.griunvald@gmail.com>
2024-03-05 20:18:11 +0200

    Add slogan, subtitle and CTA buttons to Landing page

--- client/src/components/pages/Landing.jsx
+++ client/src/components/pages/Landing.jsx
@@ -1,9 +1,20 @@
 
+import LinkButtonCTA from '../common/LinkButtonCTA';
 
-function Landing(){
-return (
+function Landing() {
+  return (
     <>
-      Landing page
+      <section>
+        <h1 className="font-bold text-gray-900 text-3xl md:text-8xl  md:leading-normal mb-10 ">Trace Your Trails: Where Memories Map Out</h1>
+        <h2 className="font-medium text-gray-900 text-3xl md:leading-loose ">From sunrise city walks to mountain mist sunsets, <br /> chart your experiences and share the story of every place.</h2>
+        <div className="flex justify-start gap-4 mt-10">
+          <LinkButtonCTA className="mx-auto block" name="Start Your Adventure" variant="primary" path="/start-trip" />
+          <LinkButtonCTA className="mx-auto block" name="Discover Journeys" variant="secondary" path="/trips-list" />
+        </div>
+      </section>
+      <section>
+
+      </section>
     </>
   );
 }
