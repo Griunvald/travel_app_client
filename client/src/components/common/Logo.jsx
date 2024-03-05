d0e984410858aceb18387f99f68badb1f03a43bf
Vitali Griunvald <me.griunvald@gmail.com>
2024-03-06 00:50:12 +0200

    Change logo link path from / to trips-list

--- client/src/components/common/Logo.jsx
+++ client/src/components/common/Logo.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 
 function Logo() {
     return(
-        <h1 className="font-pacifico text-2xl text-black"><Link to="/">Road Cronicles</Link></h1>
+        <h1 className="font-pacifico text-2xl text-black"><Link to="/trips-list">Road Cronicles</Link></h1>
     )
 };
 
