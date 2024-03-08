7a38c3f28b2233829414cb74c795071a81d66001
Vitali Griunvald <me.griunvald@gmail.com>
2024-03-08 16:55:30 +0200

    Connect FollowContext to app

--- client/src/contexts/ProvidersWrapper.jsx
+++ client/src/contexts/ProvidersWrapper.jsx
@@ -5,22 +5,25 @@ import { TagsProvider } from './TagsContext.jsx';
 import { EntryProvider } from './EntryContext.jsx';
 import { ToastProvider } from './ToastContext.jsx';
 import { ProfileProvider } from './ProfileContext.jsx';
+import { FollowProvider } from './FollowContext.jsx';
 
 export const ProvidersWrapper = ({ children }) => {
     return (
         <UserProvider>
             <ToastProvider>
-              <MenuProvider>
-                <ProfileProvider>
-                    <EntryProvider>
-                        <ImageProvider>
-                           <TagsProvider>
-                                { children }
-                           </TagsProvider>
-                        </ImageProvider>
-                    </EntryProvider>
-                </ProfileProvider>
-              </MenuProvider>
+             <FollowProvider>
+                <MenuProvider>
+                  <ProfileProvider>
+                      <EntryProvider>
+                          <ImageProvider>
+                             <TagsProvider>
+                                  { children }
+                             </TagsProvider>
+                          </ImageProvider>
+                      </EntryProvider>
+                  </ProfileProvider>
+                </MenuProvider>
+              </FollowProvider>
             </ToastProvider>
         </UserProvider>
     );
