fadf0f7688367d2a62edc597e3e84e2e312588b6
Vitali Griunvald <me.griunvald@gmail.com>
2024-03-08 06:14:32 +0200

    Add LinkNabar component

---
+++ client/src/components/common/LinkNavbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function LinkNavbar({ name, path }) {
+  return (
+    <Link to={path} className="md:max-w-max h-10 px-8 py-2 text-base font-medium rounded text-center bg-inherit text-gray-900">
+      <span className="border-b-2 border-transparent hover:border-current pb-1">
+        {name}
+      </span>
+    </Link>
+  );
+}
+
+export default LinkNavbar;
