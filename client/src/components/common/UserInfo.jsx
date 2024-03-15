78df570e16ca7d1b1775a27623e61006e804496b
Vitali Griunvald <me.griunvald@gmail.com>
2024-03-15 08:36:42 +0200

    Add return function to a UserInfo component

--- client/src/components/common/UserInfo.jsx
+++ client/src/components/common/UserInfo.jsx
@@ -4,23 +4,25 @@ import Avatar from './Avatar';
 import FollowButton from './FollowButton';
 import ThreeDotsMenu from './ThreeDotsMenu';
 
-const UserInfo = ({ avatar, username, createdAt, handleFollow, menuItems, showFollowButton }) => (
-  <div className="flex justify-between mt-2 mb-6 py-2 pb-2 items-center w-full">
-    <div className="flex gap-2 items-end">
-      <Avatar avatar={avatar} />
-      <div>
-        <p className="font-semibold text-sm">{username}</p>
-        <p className="text-sm">{createdAt}</p>
+const UserInfo = ({ avatar, username, createdAt, handleFollow, menuItems, showFollowButton }) => {
+  return (
+    <div className="flex justify-between mt-2 mb-6 py-2 pb-2 items-center w-full">
+      <div className="flex gap-2 items-end">
+        <Avatar avatar={avatar} />
+        <div>
+          <p className="font-semibold text-sm">{username}</p>
+          <p className="text-sm">{createdAt}</p>
+        </div>
       </div>
+      {showFollowButton && (
+        <div>
+          <FollowButton onClick={handleFollow} />
+          <ThreeDotsMenu menuItems={menuItems} />
+        </div>
+      )}
     </div>
-    {showFollowButton && (
-      <div>
-        <FollowButton onClick={handleFollow} />
-        <ThreeDotsMenu menuItems={menuItems} />
-      </div>
-    )}
-  </div>
-);
+  );
+};
 
 export default UserInfo;
 
