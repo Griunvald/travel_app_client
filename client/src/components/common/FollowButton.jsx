0d9e9b687867cea57a18940cfa6d0c90878c488a
Vitali Griunvald <me.griunvald@gmail.com>
2024-03-17 06:12:47 +0200

    Disable follow button if status is "Following"

--- client/src/components/common/FollowButton.jsx
+++ client/src/components/common/FollowButton.jsx
@@ -4,6 +4,7 @@ function FollowButton({ leaderId, onFollowClick, isFollowing }) {
     <button className={`md:max-w-max px-8 py-2 text-base font-medium rounded
       bg-inherit text-gray-900 border border-primary`}
       onClick={ () => onFollowClick(leaderId)}
+      disabled={isFollowing}
     >
       {isFollowing ? 'Following':'Follow'}
     </button>
