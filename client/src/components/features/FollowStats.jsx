744c769fe4d1eab8abce4d625ea9b64bd48d0452
Vitali Griunvald <me.griunvald@gmail.com>
2024-03-22 18:46:01 +0200

    Get trips count in FollowStats component on load

--- client/src/components/features/FollowStats.jsx
+++ client/src/components/features/FollowStats.jsx
@@ -1,13 +1,38 @@
 import { useFollow } from '../../contexts/FollowContext';
+import { useState, useEffect } from 'react';
 
 function FollowStats() {
   const { followStats } = useFollow();
   const { followingCount, followersCount} = followStats;
+  const [tripsCount, setTripsCount] = useState();
+
+  useEffect(() => {
+
+  const getTripsCount = async () => {
+    try {
+      const response = await fetch('http://localhost:3003/api/v1/trip/get-trips-count', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+      })
+      if (response.ok) {
+        const data = await response.json();
+        setTripsCount(data);
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+    getTripsCount();
+  }, []);
+
   return (
     <>
       <div className="flex gap-4 w-full md:w-[700px] mx-auto pt-4 md:pt-6 mb-4">
         <div>Followers: {followersCount}</div>
         <div>Following: {followingCount}</div>
+        <div>Trips: {tripsCount}</div>
       </div>
     </>
   );
