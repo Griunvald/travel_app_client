762557951393a3c6ca8854549aa78e74076472f4
Vitali Griunvald <me.griunvald@gmail.com>
2024-03-24 14:53:02 +0200

    Install and configure Redux Toolkit, add mock user slice in order to run Redux DevTools

---
+++ client/src/features/user/userSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  userInfo: null,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUserInfo: (state, action) => {
+      state.userInfo = action.payload;
+    },
+    clearUserInfo: (state) => {
+      state.userInfo = null;
+    },
+  },
+});
+
+export const { setUserInfo, clearUserInfo } = userSlice.actions;
+
+export default userSlice.reducer;
+
