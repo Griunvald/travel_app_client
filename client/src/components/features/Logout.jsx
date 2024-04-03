fd963a017595b98b8d32e8b52d6da7c575afc372
Vitali Griunvald <me.griunvald@gmail.com>
2024-04-03 09:39:14 +0300

    Replace toast context with useToast hook in Logout component

--- client/src/components/features/Logout.jsx
+++ client/src/components/features/Logout.jsx
@@ -1,24 +1,29 @@
 import { useNavigate } from 'react-router-dom';
 import { useUser } from '../../contexts/UserContext';
-import { useToast } from '../../contexts/ToastContext';
+//import { useToast } from '../../contexts/ToastContext';
 import { logoutUser } from '../../features/user/userThunks';
 import { useDispatch } from 'react-redux';
 import Button from '../common/Button';
+import { useToast } from '../../hooks/useToast';
 
 function Logout() {
   const { setUsername, setUserId, setCurrentTripId } = useUser();
-  const { showToast } = useToast();
+//  const { showToast } = useToast();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const { toast } = useToast();
 
   const handleLogout = async () => {
     try {
       const actionResult = await dispatch(logoutUser());
       if(actionResult.type.includes('fulfilled')){
       navigate('/');
-      showToast('Successfully logged out!' , {duration: 5000});
+     // showToast('Successfully logged out!' , {duration: 5000});
+      toast({message:'Successfully logged out!',  duration: 5000 });
       }
     } catch (error) {
+      toast({message:'Action failed: ' + error.message,  duration: 5000 });
       console.error('Error during logout:', error);
     }
   };
