0a2f32bea0b367728e45cd5b8673d2d49001c865
Vitali Griunvald <me.griunvald@gmail.com>
2024-04-08 15:16:08 +0300

    Replace toggleMenu function from useUser to function from menuSlice

--- client/src/components/layout/Hamburger.jsx
+++ client/src/components/layout/Hamburger.jsx
@@ -1,10 +1,15 @@
 import hamburgerIcon from '../../assets/hamburger.svg';
-import { useMenu } from  '../../contexts/MenuContext.jsx'
+//import { useMenu } from  '../../contexts/MenuContext.jsx'
+import { toggleMenu } from '../../features/menu/menuSlice';
+import { useDispatch } from 'react-redux';
 
 function Hamburger() {
-    const { toggleMenu } = useMenu();
+  const dispatch = useDispatch();
+    const  handleClick = () => {
+       dispatch(toggleMenu()); 
+    }
     return (
-        <button className="py-1.5" onClick={toggleMenu}>
+        <button className="py-1.5" onClick={handleClick}>
             <img src={hamburgerIcon} alt="hamburger menu icon" />
         </button>
     )
