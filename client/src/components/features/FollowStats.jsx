984c07ea300758692bb13d786918831307ab9739
Vitali Griunvald <me.griunvald@gmail.com>
2024-04-15 13:40:38 +0300

    Get following stats from Redux Store in FollowStats component

--- client/src/components/features/FollowStats.jsx
+++ client/src/components/features/FollowStats.jsx
@@ -1,4 +1,5 @@
 import { useFollow } from '../../contexts/FollowContext';
+import { getFollowStats } from '../../features/follow/followThunks';
 import { useState, useEffect } from 'react';
 import { getTripsCount } from '../../features/trip/tripThunks';
 import { setTripsCount } from '../../features/trip/tripSlice';
@@ -10,16 +11,20 @@ function FollowStats() {
   //const [tripsCount, setTripsCount] = useState();
   const dispatch = useDispatch();
   const { tripsCount } = useSelector(store => store.trip);
+  const { followingCount, followersCount } = useSelector(store => store.follow.followStats);
 
   useEffect(() => {
     dispatch(getTripsCount());
     dispatch(setTripsCount());
+    dispatch(getFollowStats());
   }, [dispatch]);
 
   return (
     <>
       <div className="flex gap-4 w-full md:w-[700px] mx-auto pt-4 md:pt-6 mb-4">
         <div>Trips: {tripsCount}</div>
+        <div>Followers: {followersCount}</div>
+        <div>Following: {followingCount}</div>
       </div>
     </>
   );
@@ -28,5 +33,6 @@ function FollowStats() {
 export default FollowStats;
 
 
+// TODO: Replace follow context with Slice
         //<div>Followers: {followersCount}</div>
         //<div>Following: {followingCount}</div>
