54dd1ad9222aacc93edd72692b4fd8992fdb6ce9
Vitali Griunvald <me.griunvald@gmail.com>
2024-04-15 11:24:01 +0300

    Remove commented code from EntryList component

--- client/src/components/features/EntryList.jsx
+++ client/src/components/features/EntryList.jsx
@@ -1,46 +1,38 @@
-import { useState, useEffect } from 'react';
-//import { useUser } from '../../contexts/UserContext';
-//import { useEntries } from '../../contexts/EntryContext';
+import { useEffect } from 'react';
 import Entry from './Entry';
 import { getEntryList } from '../../features/entry/entryThunks';
 import { setEntryList } from '../../features/entry/entrySlice';
 import { useSelector, useDispatch } from 'react-redux';
 
-function EntryList(){
-    //const { userId } = useUser();
+function EntryList() {
   const dispatch = useDispatch();
   const userId = useSelector(store => store.user.userId);
   const entryList = useSelector(store => store.entry.entryList);
-    //const { entryList, refreshEntries } = useEntries([]);
-
-  //useEffect(() => {
-  //    refreshEntries(); 
-  //}, [userId]);
 
   useEffect(() => {
-    if(userId){
+    if (userId) {
       dispatch(getEntryList(userId));
       dispatch(setEntryList());
     }
   }, [userId, useDispatch]);
 
-    return(
-        <div>{
-            entryList && (
-                Array.isArray(entryList) && entryList.map(entry => (
-                <div key={entry.id}>
-                    <Entry  
-                      createdAt={entry.createdAt}
-                      textValue={entry.textValue}
-                      urlValue={entry.urlValue}
-                      recordTags={entry.recordTags}
-                    />
-                </div>
+  return (
+    <div>{
+      entryList && (
+        Array.isArray(entryList) && entryList.map(entry => (
+          <div key={entry.id}>
+            <Entry
+              createdAt={entry.createdAt}
+              textValue={entry.textValue}
+              urlValue={entry.urlValue}
+              recordTags={entry.recordTags}
+            />
+          </div>
 
-                ))
-            )
-        }</div>
-    )
+        ))
+      )
+    }</div>
+  )
 }
 
 export default EntryList;
