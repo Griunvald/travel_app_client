f08c30b42ebbbf9891d4c5cd3ae72dd193a0b4b5
Vitali Griunvald <me.griunvald@gmail.com>
2024-04-15 10:15:55 +0300

    Replace entryContext in a EntryList component to Redux Slice

--- client/src/components/features/EntryList.jsx
+++ client/src/components/features/EntryList.jsx
@@ -1,15 +1,28 @@
 import { useState, useEffect } from 'react';
-import { useUser } from '../../contexts/UserContext';
-import { useEntries } from '../../contexts/EntryContext';
+//import { useUser } from '../../contexts/UserContext';
+//import { useEntries } from '../../contexts/EntryContext';
 import Entry from './Entry';
+import { getEntryList } from '../../features/entry/entryThunks';
+import { setEntryList } from '../../features/entry/entrySlice';
+import { useSelector, useDispatch } from 'react-redux';
 
 function EntryList(){
-    const { userId } = useUser();
-    const { entryList, refreshEntries } = useEntries([]);
+    //const { userId } = useUser();
+  const dispatch = useDispatch();
+  const userId = useSelector(store => store.user.userId);
+  const entryList = useSelector(store => store.entry.entryList);
+    //const { entryList, refreshEntries } = useEntries([]);
+
+  //useEffect(() => {
+  //    refreshEntries(); 
+  //}, [userId]);
 
   useEffect(() => {
-      refreshEntries(); 
-  }, [userId]);
+    if(userId){
+      dispatch(getEntryList(userId));
+      dispatch(setEntryList());
+    }
+  }, [userId, useDispatch]);
 
     return(
         <div>{
