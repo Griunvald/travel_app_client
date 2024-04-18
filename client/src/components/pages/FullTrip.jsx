d8f191847401dd81c6a75e6eadbc0b1b49134a69
Vitali Griunvald <me.griunvald@gmail.com>
2024-04-18 11:49:31 +0300

    Remove comments and unused code in FullTrip component

--- client/src/components/pages/FullTrip.jsx
+++ client/src/components/pages/FullTrip.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import format from 'date-fns/format';
@@ -8,17 +8,14 @@ import { getFollowingUsers } from '../../features/follow/followThunks';
 import { getFullTrip } from '../../features/trip/tripThunks';
 
 function FullTrip() {
-//  const [tripDetails, setTripDetails] = useState({});
- // const [entryList, setEntryList] = useState([]);
   const { userId } = useParams();
-  //const [formattedDate, setFormattedDate] = useState(null);
   const dispatch = useDispatch();
   const leaderId = useSelector(store => store.follow.leaderId);
   const ID = Number(userId);
 
-const { tripDetails, entryList } = useSelector((state) => state.trip);
+  const { tripDetails, entryList } = useSelector((state) => state.trip);
 
-const formattedDate = tripDetails.createdAt ? format(new Date(tripDetails.createdAt), "MMMM do, yyyy, hh:mm a") : null;
+  const formattedDate = tripDetails.createdAt ? format(new Date(tripDetails.createdAt), "MMMM do, yyyy, hh:mm a") : null;
 
   useEffect(() => {
     dispatch(getFollowingUsers());
@@ -27,7 +24,7 @@ const formattedDate = tripDetails.createdAt ? format(new Date(tripDetails.create
   useEffect(() => {
     dispatch(getFullTrip(userId));
   }, [userId]);
-  
+
   return (
     <>
       {tripDetails &&
