dbc72f40c646ee10033ca607fb9b8f5b6f219de7
Vitali Griunvald <me.griunvald@gmail.com>
2024-04-21 20:04:50 +0300

    Remove logs

--- client/src/components/features/About.jsx
+++ client/src/components/features/About.jsx
@@ -7,34 +7,32 @@ import Button from '../common/Button';
 
 function About() {
   const about = useSelector(store => store.profile.about);
-  console.log(about);
   const userId = useSelector(store => store.user.userId);
   const dispatch = useDispatch();
 
-    const handleChange = (e) => {
-      dispatch(setAbout(e.target.value));
-    };
+  const handleChange = (e) => {
+    dispatch(setAbout(e.target.value));
+  };
 
-    const handleSubmit = async () => {
-      await dispatch(updateProfile({userId, data: {about} }))
-      dispatch(getProfile(userId));
+  const handleSubmit = async () => {
+    await dispatch(updateProfile({ userId, data: { about } }))
+    dispatch(getProfile(userId));
+  }
 
-    }
-
-    return (
+  return (
     <>
-        <h1>About page</h1>
-        <Form onSubmit={handleSubmit}>
+      <h1>About page</h1>
+      <Form onSubmit={handleSubmit}>
         <Textarea
           value={about}
           onChange={handleChange}
         />
         <div className="flex flex-col md:flex-row md:justify-end">
-            <Button name="Save" variant="primary" action="submit"/>
+          <Button name="Save" variant="primary" action="submit" />
         </div>
       </Form>
     </>
-    );
+  );
 };
 
 export default About;
