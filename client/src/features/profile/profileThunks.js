39403c4b65723da08618bf721c38a33e92466aa3
Vitali Griunvald <me.griunvald@gmail.com>
2024-04-26 10:13:04 +0300

    Change /get-profile api call and refactor getProfile function

--- client/src/features/profile/profileThunks.js
+++ client/src/features/profile/profileThunks.js
@@ -2,43 +2,49 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const getProfile = createAsyncThunk(
   'profile/getProfile',
-  async (userId, thunkApi) => {
-      try {
-        const response = await fetch(`http://localhost:3003/api/v1/user/profile/get-profile?userId=${userId}`);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
+  async (_, thunkApi) => {
+    try {
+      const response = await fetch(`http://localhost:3003/api/v1/users/profile`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
         }
-        const data = await response.json();
-        return data; 
-      } catch (error) {
-          return thunkApi.rejectWithValue(error.message);
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
   }
 );
 
 export const updateProfile = createAsyncThunk(
   'profile/updateProfile',
-  async ({userId, data}, thunkApi) => {
-    const url = `http://localhost:3003/api/v1/user/profile/update-profile?userId=${userId}`; 
-      try {
-        const response = await fetch(url, {
-          method: 'PUT',
-          credentials: 'include',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data), 
-        });
+  async ({ userId, data }, thunkApi) => {
+    const url = `http://localhost:3003/api/v1/users/profile`;
+    try {
+      const response = await fetch(url, {
+        method: 'PUT',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const result = await response.json();
-        return result;
-      } catch (error) {
-        return thunkApi.rejectWithValue(error.message);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
+
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
   }
 );
 
