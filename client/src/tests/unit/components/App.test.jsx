48ac1b4e30470ea810ad65f72dbaea5bb83208ed
Vitali Griunvald <me.griunvald@gmail.com>
2024-04-27 19:37:49 +0300

    Fix warnings caused by Logo component

--- client/src/tests/unit/components/App.test.jsx
+++ client/src/tests/unit/components/App.test.jsx
@@ -1,27 +1,26 @@
 import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';
-import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from '@/App';
 import { makeTestStore } from '@/store';
 
 describe('App component', () => {
-  it('initializes user and handles routes correctly', async () => {
-    const store = makeTestStore({
-      preloadedState: {
-        user: { userId: null },
-        trip: { currentTrip: null }
-      }
-    });
+  it('renders correctly with initial null user and trip data', async () => {
+    const initialState = {
+      user: { userId: null, username: null },
+      trip: { currentTrip: null },
+    };
 
-    const router = createMemoryRouter([
-      { path: '/', element: <App /> }
-    ]);
+    const store = makeTestStore(initialState);
 
     render(
       <Provider store={store}>
-        <RouterProvider router={router} />
+        <App />
       </Provider>
     );
+
+    await waitFor(() => {
+      expect(screen.getByText('Road Cronicles')).toBeInTheDocument();
+    }, { timeout: 5000 });
   });
 });
 
