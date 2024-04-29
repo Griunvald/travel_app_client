b4c3b446503e72577e35a5bc186c958d06ef37fd
Vitali Griunvald <me.griunvald@gmail.com>
2024-04-29 11:48:53 +0300

    Add test for the Join button and Join from for desktop

--- client/src/tests/integration/components/Navbar.integration.test.jsx
+++ client/src/tests/integration/components/Navbar.integration.test.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { createMemoryRouter, RouterProvider, Outlet } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Navbar from '@/components/layout/Navbar';
+import Join from '@/components/pages/Join';
+import Login from '@/components/pages/Login';
+import NotFound from '@/components/pages/NotFound';
+import Landing from '@/components/pages/Landing';
+import TripsList from '@/components/pages/TripsList';
+import Sidebar from '@/components/layout/Sidebar'
+import Footer from '@/components/layout/Footer'
+import App from '@/App';
 import { makeTestStore } from '@/store';
 
+
 describe('Navbar with Logo component', () => {
+
+  beforeEach(() => {
+    global.window.innerWidth = 768;
+    global.window.dispatchEvent(new Event('resize'));
+  });
+
   it('shows correct link in Logo for non logged in user', () => {
     const initialState = {
       user: { userId: null, username: null },
@@ -15,7 +30,7 @@ describe('Navbar with Logo component', () => {
 
     const router = createMemoryRouter([
       { path: "/", element: <Navbar /> },
-      { path: "/landing", element: <div>Landing Page</div> },
+      { path: "/landing", element: <div></div> },
       { path: "*", element: <div>404 Not Found</div> }
     ]);
 
@@ -56,6 +71,77 @@ describe('Navbar with Logo component', () => {
     const logoLink = screen.getByRole('link', { name: /Road Cronicles/i });
     expect(logoLink).toHaveAttribute('href', '/trips-list');
   });
+
+  it('open "Join" form on "Join" button click', async () => {
+
+    const initialState = {
+      user: { userId: null, username: null },
+      trip: { currentTripId: null }
+    };
+
+    const store = makeTestStore(initialState);
+
+    const NavbarWrapper = () => {
+      return (
+        <>
+          <Navbar />
+          <Sidebar />
+          <main>
+            <div>
+              <Outlet />
+            </div>
+          </main>
+          <Footer />
+        </>
+      )
+    }
+
+    const router = createMemoryRouter([
+      {
+        element: <NavbarWrapper />,
+        children: [
+          {
+            path: '/',
+            element: <div>Landing</div>
+          },
+          {
+            path: '/landing',
+            element: <div>Landing</div>
+          },
+          {
+            path: '/trips-list',
+            element: <TripsList />
+          },
+          {
+            path: '/login',
+            element: <Login />
+          },
+          {
+            path: '/join',
+            element: <Join />
+          },
+          {
+            path: '*',
+            element: <NotFound />
+          },
+        ]
+      }
+    ])
+
+    render(
+      <Provider store={store}>
+        <RouterProvider router={router}>
+          <Navbar />
+          <App />
+        </RouterProvider>
+      </Provider>
+    );
+
+    const link = screen.getByRole('link', { name: /Join/i });
+    fireEvent.click(link);
+    const joinText = await screen.findByText('Join Road Cronicles', {}, { timeout: 5000 });
+    expect(joinText).toBeInTheDocument();
+  })
 });
 
 
