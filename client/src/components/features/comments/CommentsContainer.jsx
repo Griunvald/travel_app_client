f6bfe8c1e6f96c3dc36addcdd984ddc839a9d30b
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-04 12:37:53 +0300

    Import Comment component to CommentConteiner, add comments array as placeholder

--- client/src/components/features/comments/CommentsContainer.jsx
+++ client/src/components/features/comments/CommentsContainer.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Textarea from "../../common/Textarea";
 import Form from "../../common/Form";
+import Comment from "./Comment";
 
 function CommentsContainer() {
 
@@ -10,19 +11,52 @@ function CommentsContainer() {
     setComment(e.target.value);
   }
 
+  const comments = [
+    {
+      id: 1,
+      userId: 1,
+      username: "Blue",
+      createdAt: '2024-05-03 14:48:49.146989',
+      body: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, voluptates fugiat hic explicabo labore fuga.
+Eveniet fugit explicabo temporibus in consequuntur sunt voluptates molestiae esse non dolorem consequatur, odit
+nam ab possimus dolor voluptatum et, maxime modi eius voluptatibus voluptas doloremque, magni impedit?`
+    },
+    {
+      id: 2,
+      userId: 2,
+      username: "Green",
+      createdAt: '2024-04-30 14:48:49.146989',
+      body: `Lorem ipsum dolor sit amet, consectetur adipisicing elit!! .`
+    },
+    {
+      id: 3,
+      userId: 3,
+      username: "Pink",
+      createdAt: '2024-04-25 14:48:49.146989',
+      body: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, voluptates fugiat hic explicabo labore fuga.
+Eveniet fugit explicabo temporibus in consequuntur sunt voluptates molestiae esse non dolorem consequatur, odit
+nam ab possimus dolor voluptatum et.`
+    },
+  ]
+
   return (
     <>
-      <div className="w-full md:w-[700px] mx-auto">
-        <h2 className="font-medium text-2xl text-center mb-6">Add a comment</h2>
+      <div className="w-full md:w-[700px] mx-auto mt-12">
         <Form onSubmit={handleSubmit}>
           <Textarea
-            rows="1"
+            rows="2"
             maxLength={500}
             showCharacterCount={true}
             value={comment}
+            placeholder="Add a comment"
             onChange={handleChange}
           />
         </Form>
+        <div className="grid grid-cols-1 divide-y mt-4">
+          {comments &&
+            comments.map(comment => <Comment comment={comment} />)
+          }
+        </div>
       </div >
     </>
   );
