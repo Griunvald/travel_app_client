f3864baa077d577ae68a776bf3907d5127894fc3
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-04 18:08:03 +0300

    Use comment thunk in Comment component, use hardcoded tripId

--- client/src/components/features/comments/Comment.jsx
+++ client/src/components/features/comments/Comment.jsx
@@ -4,8 +4,7 @@ import { timeAgo } from "../../../utils/date";
 
 const Comment = ({ comment }) => {
   const { userId } = useSelector(store => store.user)
-  const canEdit = comment.userId == userId;
-  console.log(comment.id, comment.userId, userId);
+  const canEdit = comment.user_id == userId;
   return (
     <div className="bg-white border-black border-solid  p-4 mb-4">
       <div className="flex items-start space-x-4">
@@ -13,7 +12,7 @@ const Comment = ({ comment }) => {
         <div className="flex-1">
           <div className="flex items-center justify-between">
             <h5 className="font-bold text-sm">{comment.username}</h5>
-            <span className="text-xs text-gray-800">{timeAgo(comment.createdAt)}</span>
+            <span className="text-xs text-gray-800">{timeAgo(comment.created_at)}</span>
           </div>
           <p className="text-base font-normal mt-2">{comment.body}</p>
           <div className="flex space-x-2 mt-2">
--- client/src/components/features/comments/CommentsContainer.jsx
+++ client/src/components/features/comments/CommentsContainer.jsx
@@ -1,43 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import Textarea from "../../common/Textarea";
 import Form from "../../common/Form";
 import Comment from "./Comment";
-
+import { getComments } from '../../../features/comment/commentThunks.js';
 function CommentsContainer() {
 
   const [comment, setComment] = useState("");
+
   const handleSubmit = () => { }
   const handleChange = (e) => {
     setComment(e.target.value);
   }
 
-  const comments = [
-    {
-      id: 1,
-      userId: 1,
-      username: "Blue",
-      createdAt: '2024-05-03 14:48:49.146989',
-      body: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, voluptates fugiat hic explicabo labore fuga.
-Eveniet fugit explicabo temporibus in consequuntur sunt voluptates molestiae esse non dolorem consequatur, odit
-nam ab possimus dolor voluptatum et, maxime modi eius voluptatibus voluptas doloremque, magni impedit?`
-    },
-    {
-      id: 2,
-      userId: 2,
-      username: "Green",
-      createdAt: '2024-04-30 14:48:49.146989',
-      body: `Lorem ipsum dolor sit amet, consectetur adipisicing elit!! .`
-    },
-    {
-      id: 3,
-      userId: 3,
-      username: "Pink",
-      createdAt: '2024-04-25 14:48:49.146989',
-      body: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, voluptates fugiat hic explicabo labore fuga.
-Eveniet fugit explicabo temporibus in consequuntur sunt voluptates molestiae esse non dolorem consequatur, odit
-nam ab possimus dolor voluptatum et.`
-    },
-  ]
+  const dispatch = useDispatch();
+  const { comments } = useSelector(store => store.comment.commentsList)
+  const tripId = 1;
+
+  useEffect(() => {
+    async function fetchData() {
+      await dispatch(getComments(tripId))
+    }
+    fetchData();
+  }, [tripId]);
 
   return (
     <>
