66b7addc731c3c113438a03575e7d0f6e90ac530
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-06 18:28:07 +0300

    Minor changes in editComment thunk

--- client/src/features/comment/commentThunks.js
+++ client/src/features/comment/commentThunks.js
@@ -65,28 +65,28 @@ export const deleteComment = createAsyncThunk(
   }
 );
 
-
-
 export const editComment = createAsyncThunk(
   'comment/editComment',
   async ({ commentId, commentOwner, comment }, thunkApi) => {
-    const url = `http://localhost:3003/api/v1/comments/${commentId}/${commentOwner}`
+    const url = `http://localhost:3003/api/v1/comments/${commentId}/${commentOwner}`;
     try {
       const response = await fetch(url, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
-        //FIXME:
         body: JSON.stringify({ body: comment })
       });
       if (response.ok) {
-        const comment = await response.json()
-        return comment;
+        const updatedComment = await response.json();
+        return updatedComment;
       } else {
-        console.error('Failed to delete comment');
+        console.error('Failed to update comment');
+        return thunkApi.rejectWithValue('Failed to update comment');
       }
     } catch (err) {
+      console.error('Error updating comment:', err);
       return thunkApi.rejectWithValue(err.message);
     }
   }
 );
+
