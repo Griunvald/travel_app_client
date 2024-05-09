e56404fa6e55643cae74aa1b49fdd4f3a57c03c3
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-09 20:17:20 +0300

    Add props to Like component, add addLike and removeLike handlers
    Dynamically dispatch thunk functions

--- client/src/components/common/Like.jsx
+++ client/src/components/common/Like.jsx
@@ -1,11 +1,26 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addLike, removeLike } from '../../features/like/likeThunks';
 import LikeIcon from '../../assets/Like.svg'
 import UnlikeIcon from '../../assets/Unlike.svg'
 
-function Like() {
+function Like({ type, itemId }) {
+  console.log(type, itemId);
   const [like, setLike] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleAddLike = async () => {
+    await dispatch(addLike({ type, itemId }))
+    setLike(true);
+  }
+
+  const handleRemoveLike = async () => {
+    await dispatch(removeLike({ type, itemId }))
+    setLike(false);
+  }
+
   return (
-    <div onClick={() => setLike(!like)}>
+    <div onClick={like ? handleRemoveLike : handleAddLike}>
       {like ? <img className='cursor-pointer' src={LikeIcon} /> : <img className='cursor-pointer' src={UnlikeIcon} />}
     </div>
   )
