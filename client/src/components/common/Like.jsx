f6b0c3ab23af5400a6cb2e187ad925b9766250ce
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-11 17:29:28 +0300

    Edit Like component - show likes count for each item.

--- client/src/components/common/Like.jsx
+++ client/src/components/common/Like.jsx
@@ -1,31 +1,39 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addLike, removeLike } from '../../features/like/likeThunks';
-import LikeIcon from '../../assets/Like.svg'
-import UnlikeIcon from '../../assets/Unlike.svg'
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addLike, removeLike, getItemLikesCountListByType } from '../../features/like/likeThunks';
+import LikeIcon from '../../assets/Like.svg';
+import UnlikeIcon from '../../assets/Unlike.svg';
 
 function Like({ type, itemId }) {
-  console.log(type, itemId);
   const [like, setLike] = useState(false);
   const dispatch = useDispatch();
+  const { likesCountList } = useSelector(store => store.like);
+
+  useEffect(() => {
+    dispatch(getItemLikesCountListByType({ type }));
+  }, [dispatch, type]);
+
+  const itemLikeCount = likesCountList.find(item => item.type_id === itemId);
 
   const handleAddLike = async () => {
-    await dispatch(addLike({ type, itemId }))
+    await dispatch(addLike({ type, itemId }));
+    await dispatch(getItemLikesCountListByType({ type }));
     setLike(true);
-  }
+  };
 
   const handleRemoveLike = async () => {
-    await dispatch(removeLike({ type, itemId }))
+    await dispatch(removeLike({ type, itemId }));
+    await dispatch(getItemLikesCountListByType({ type }));
     setLike(false);
-  }
+  };
 
   return (
     <div onClick={like ? handleRemoveLike : handleAddLike}>
-      {like ? <img className='cursor-pointer' src={LikeIcon} /> : <img className='cursor-pointer' src={UnlikeIcon} />}
+      {like ? <img className='cursor-pointer' src={LikeIcon} alt="Unlike" /> : <img className='cursor-pointer' src={UnlikeIcon} alt="Like" />}
+      <p>{itemLikeCount ? itemLikeCount.item_likes_count : 0}</p>
     </div>
-  )
+  );
 }
 
 export default Like;
-
 
