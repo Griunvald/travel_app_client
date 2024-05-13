30a333b65a714ef0989cc98a20a82745604a98a8
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-13 06:13:13 +0300

    Add avatar URL from Store to Avatar component

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,12 +1,16 @@
 import avatarIcon from '../../assets/avatar.svg';
+import { useSelector } from 'react-redux';
 
 function Avatar({ avatar, size }) {
-    const sizeClass = size === 'big' ? 'h-12 w-12' : 'h-10 w-10'
-    return (
-        <div>
-           <img className={`${sizeClass} object-cover rounded-full`} src={avatar ? avatar : avatarIcon} alt="user avatar" /> 
-        </div>
-    );
+  const avatarUrl = useSelector(store => store.profile.avatar);
+  const sizeClass = size === 'big' ? 'h-12 w-12' : 'h-10 w-10'
+  const url = `https://travel-app-dev.s3.il-central-1.amazonaws.com/${avatarUrl}`;
+  console.log(url);
+  return (
+    <div>
+      <img className={`${sizeClass} object-cover rounded-full`} src={avatarUrl ? url : avatarIcon} alt="user avatar" />
+    </div>
+  );
 };
 
 export default Avatar;
