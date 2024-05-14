3ce92c1f0e4e1c94d58e71e1e44234e331a33358
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-14 09:00:47 +0300

    Pass avatar url to Avatar component via props instead of directly within component

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,14 +1,12 @@
 import avatarIcon from '../../assets/avatar.svg';
-import { useSelector } from 'react-redux';
 
 function Avatar({ avatar, size }) {
-  const avatarUrl = useSelector(store => store.profile.avatar);
   const sizeClass = size === 'big' ? 'h-12 w-12' : 'h-10 w-10'
-  const url = `https://travel-app-dev.s3.il-central-1.amazonaws.com/${avatarUrl}`;
+  const url = `https://travel-app-dev.s3.il-central-1.amazonaws.com/${avatar}`;
   console.log(url);
   return (
     <div>
-      <img className={`${sizeClass} object-cover rounded-full`} src={avatarUrl ? url : avatarIcon} alt="user avatar" />
+      <img className={`${sizeClass} object-cover rounded-full`} src={avatar ? url : avatarIcon} alt="user avatar" />
     </div>
   );
 };
