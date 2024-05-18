8e4cd334425b3994aedb9869de1868fe10b43b6a
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-18 09:58:36 +0300

    Remove useEffect from Profile component for fetching user profile
    Get pfofile from state on app load insted

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getProfile } from '../../features/profile/profileThunks';
+//import { getProfileAndSaveToLocalStorage, getProfileFromLocalStorage } from '../../features/profile/profileThunks';
 import { getFollowStats } from '../../features/follow/followThunks';
 import About from '../features/About';
 import Logout from '../features/Logout';
@@ -12,15 +12,10 @@ function Profile() {
   const userId = useSelector(store => store.user.userId);
   useEffect(() => {
     if (userId) {
+      //TODO: Do I need dispatch here?
       getFollowStats();
     }
   }, []);
-
-  useEffect(() => {
-    if (userId) {
-      dispatch(getProfile());
-    }
-  }, [userId]);
 
   return (
     <div className="w-full md:w-[700px] mx-auto pt-4 md:pt-6 mb-4">
