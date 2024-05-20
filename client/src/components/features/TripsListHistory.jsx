a442aaf9bbcd088e810073cf7d928a92912879d9
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-20 09:52:13 +0300

    Render basic data for TripList in TripsListHistory with no styling

--- client/src/components/features/TripsListHistory.jsx
+++ client/src/components/features/TripsListHistory.jsx
@@ -1,6 +1,27 @@
+import { getTripsList } from "../../features/trip/tripThunks";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
 function TripsListHistory() {
+  const dispatch = useDispatch();
+  const { tripsList } = useSelector(store => store.trip)
+
+  useEffect(() => {
+    const getList = async () => {
+      await dispatch(getTripsList());
+    }
+    getList();
+  }, []);
+
   return (
-    <div>Trips list history</div>
+    <div>
+      {tripsList && tripsList.map(trip => (
+        <div>
+          <p>{trip.title}</p>
+          <p>{trip.status}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
