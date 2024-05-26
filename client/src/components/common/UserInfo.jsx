aa0a39251e78c9a8929999c61c434ae5c8f0a56e
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-26 21:38:56 +0300

    Add AboutPreview component, import to UserInfo component

--- client/src/components/common/UserInfo.jsx
+++ client/src/components/common/UserInfo.jsx
@@ -1,24 +1,30 @@
-// UserInfo.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from './Avatar';
 import FollowButton from './FollowButton';
 import ThreeDotsMenu from './ThreeDotsMenu';
+import AboutPreview from '../features/AboutPreview';
 
 const UserInfo = ({ leader, avatar, username, createdAt, menuItems, showFollowButton, onFollowClick, followingUsers }) => {
+  const [hover, setHover] = useState(false);
   const isFollowing = followingUsers.includes(leader);
   return (
-    <div className="flex justify-between mt-2 mb-6 py-2 pb-2 items-center w-full">
+    <div className="relative flex justify-between mt-2 mb-6 py-2 pb-2 items-center w-full">
       <div className="flex gap-2 items-end">
         <Avatar avatar={avatar} />
-        <div>
+        <div
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          className="relative"
+        >
           <p className="font-semibold text-sm">{username}</p>
+          {hover && <AboutPreview />}
           <p className="text-sm">{createdAt}</p>
         </div>
       </div>
       {showFollowButton && (
         <div>
           <FollowButton leaderId={leader} onFollowClick={onFollowClick} isFollowing={isFollowing} />
-          { isFollowing ? <ThreeDotsMenu menuItems={menuItems} /> : null }
+          {isFollowing && <ThreeDotsMenu menuItems={menuItems} />}
         </div>
       )}
     </div>
