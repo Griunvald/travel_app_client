ceadfa1ba1cc7cd8c2d9a3baeade7f8446f52021
Vitali Griunvald <me.griunvald@gmail.com>
2024-06-02 17:23:48 +0300

    Scroll AboutMe page to top on load

--- client/src/components/pages/AboutMe.jsx
+++ client/src/components/pages/AboutMe.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import { useEffect } from 'react';
 
 function About() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="max-w-4xl mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6">About</h1>
