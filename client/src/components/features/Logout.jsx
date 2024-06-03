f1824d5fba1d2be69b10564f7e5ba7e3396b1e99
Vitali Griunvald <me.griunvald@gmail.com>
2024-06-03 11:36:03 +0300

    Remove comments

--- client/src/components/features/Logout.jsx
+++ client/src/components/features/Logout.jsx
@@ -1,14 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-//import { useUser } from '../../contexts/UserContext';
-//import { useToast } from '../../contexts/ToastContext';
 import { logoutUser } from '../../features/user/userThunks';
 import { useDispatch } from 'react-redux';
 import Button from '../common/Button';
 import { useToast } from '../../hooks/useToast';
 
 function Logout() {
-  //const { setUsername, setUserId, setCurrentTripId } = useUser();
-  //  const { showToast } = useToast();
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -20,7 +16,6 @@ function Logout() {
       if (actionResult.type.includes('fulfilled')) {
         localStorage.removeItem('profile');
         navigate('/');
-        // showToast('Successfully logged out!' , {duration: 5000});
         toast({ message: 'Successfully logged out!', duration: 5000 });
       }
     } catch (error) {
