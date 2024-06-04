d841ae7dd900890aeb000d6556964483c52e526a
Vitali Griunvald <me.griunvald@gmail.com>
2024-06-04 05:09:09 +0300

    Add onClick to LinkNavbar component

--- client/src/components/common/LinkNavbar.jsx
+++ client/src/components/common/LinkNavbar.jsx
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom';
 
-function LinkNavbar({ name, path }) {
+function LinkNavbar({ name, path, onClick }) {
   return (
-    <Link to={path} className="md:max-w-max h-10 px-8 py-2 text-base font-medium rounded text-center bg-inherit text-gray-900">
+    <Link to={path} className="md:max-w-max h-10 px-8 py-2 text-base font-medium 
+      rounded text-center bg-inherit text-gray-900" onClick={onClick}>
       <span className="border-b-2 border-transparent hover:border-current pb-1">
         {name}
       </span>
