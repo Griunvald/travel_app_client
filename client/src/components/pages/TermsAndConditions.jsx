037d038a327a8654699759f502764926b5ccc54f
Vitali Griunvald <me.griunvald@gmail.com>
2024-06-09 19:46:43 +0300

    Minor change

--- client/src/components/pages/TermsAndConditions.jsx
+++ client/src/components/pages/TermsAndConditions.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 
-const TermsAndConditions = () => {
+function TermsAndConditions () {
   return (
     <div className="max-w-4xl mx-auto p-6">
       <h1 className="text-2xl font-bold mb-6">Terms and Conditions</h1>
