26800536335b39f7720c98e59af18085f38bfca4
Vitali Griunvald <me.griunvald@gmail.com>
2024-06-09 21:42:28 +0300

    Update Button component - add disabled state

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -2,13 +2,13 @@ function Button({ name, variant, action, disabled, inProgressText, onClick }) {
   return (
     <button 
       className={`md:max-w-max px-8 py-2 text-base font-medium rounded 
-        ${variant === 'primary' ? 'bg-accent text-white' :
-                'bg-inherit text-gray-900 border border-primary'}`} 
+        ${variant === 'primary' ? 'bg-accent text-white' : 'bg-inherit text-gray-900 border border-primary'}
+        ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`} 
       type={action} 
       disabled={disabled}
       onClick={onClick}
     >
-      {disabled ? inProgressText : name}
+      {disabled && inProgressText ? inProgressText : name}
     </button>
   );
 }
