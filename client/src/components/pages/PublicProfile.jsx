f3dd6ef869365839e65ebb7a42d3ea1bdf4b608b
Vitali Griunvald <me.griunvald@gmail.com>
2024-06-17 20:50:15 +0300

    Add styles to PublicProfile, render about text

--- client/src/components/pages/PublicProfile.jsx
+++ client/src/components/pages/PublicProfile.jsx
@@ -6,13 +6,15 @@ import { useParams } from 'react-router-dom';
 function PublicProfile(){
   const { userId } = useParams();
   const dispatch = useDispatch();
+  const { about } = useSelector(store => store.profile.publicProfile)
 
   useEffect(() => {
     dispatch(getProfile(userId));
   }, [userId]);
   return(
-     <div>
-      Public profile
+    <div className="w-full md:w-[700px] mx-auto pt-4 md:pt-6 mb-4">
+      <h2 className="font-medium text-xl text-left mb-6">About me</h2>
+      <p>{about}</p>
     </div>
   );
 
