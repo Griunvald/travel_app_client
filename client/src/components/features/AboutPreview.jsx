110fe57da4146178860bee7ea33ac7d3576f8bc5
Vitali Griunvald <me.griunvald@gmail.com>
2024-06-18 20:02:05 +0300

    Replace Link in AboutPreview to button with handler that use useNavigate hook

--- client/src/components/features/AboutPreview.jsx
+++ client/src/components/features/AboutPreview.jsx
@@ -1,15 +1,19 @@
-import Link from '../common/Link';
-
+import { useNavigate } from 'react-router-dom';
 function AboutPreview({ about, leader }) {
   if (!about) return null;
+  const navigate = useNavigate();
 
   const truncatedAbout = about.length > 70 ? `${about.substring(0, 70)}...` : about;
+
+  const handleReadMoreClick = () => {
+    navigate(`/public-profile/${leader}`, { state: { from: window.location.pathname } });
+  };
 
   return (
     <div className="absolute w-80 p-4 bg-white border border-gray-300 shadow-lg z-50">
       <p>{truncatedAbout}</p>
       {about.length > 70 && (
-        <Link name="Read more" path={`/public-profile/${leader}`} />
+       <button onClick={handleReadMoreClick} className="underline decoration-solid font-medium">Read more</button>
       )}
     </div>
   );
