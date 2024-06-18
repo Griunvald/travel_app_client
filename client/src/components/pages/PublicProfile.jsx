7ccad58d61dc0156f09741c8c22bee410899e1c2
Vitali Griunvald <me.griunvald@gmail.com>
2024-06-18 20:13:46 +0300

    Replace Link in Public profile with button and handler that use useNavigate hook and redirect back to route in comes from

--- client/src/components/pages/PublicProfile.jsx
+++ client/src/components/pages/PublicProfile.jsx
@@ -1,20 +1,33 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProfile } from '../../features/profile/profileThunks.js'
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
 
 function PublicProfile(){
   const { userId } = useParams();
   const dispatch = useDispatch();
   const { about } = useSelector(store => store.profile.publicProfile)
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     dispatch(getProfile(userId));
   }, [userId]);
+
+    const handleBackClick = () => {
+    const { from } = location.state || {};
+    if (from) {
+      navigate(from);
+    } else {
+      navigate('/trips-list');
+    }
+  };
+
   return(
     <div className="w-full md:w-[700px] mx-auto pt-4 md:pt-6 mb-4">
       <h2 className="font-medium text-xl text-left mb-6">About me</h2>
       <p>{about}</p>
+       <button onClick={handleBackClick} className="underline decoration-solid font-medium mt-2">Back</button>
     </div>
   );
 
