573ae86b898d082c7ba15f46861c6b4f2bbd436a
Vitali Griunvald <me.griunvald@gmail.com>
2024-06-19 12:23:32 +0300

    Add hybrid fixed/static button to FullTrip component that backlink to /trips-list route

--- client/src/components/pages/FullTrip.jsx
+++ client/src/components/pages/FullTrip.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import format from 'date-fns/format';
 import TripPreview from '../features/TripPreview';
@@ -11,16 +11,18 @@ import { getFullTrip } from '../../features/trip/tripThunks';
 function FullTrip() {
   const { userId } = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const leaderId = useSelector(store => store.follow.leaderId);
   const username = useSelector(store => store.user.username);
   const ID = Number(userId);
 
   const { tripDetails, entryList } = useSelector((state) => state.trip);
+  const [buttonBottom, setButtonBottom] = useState(0);
 
   const formattedDate = tripDetails.createdAt ? format(new Date(tripDetails.createdAt), "MMMM do, yyyy, hh:mm a") : null;
 
   useEffect(() => {
-    if(username){
+    if (username) {
       dispatch(getFollowingUsers());
     }
   }, [leaderId]);
@@ -33,8 +35,33 @@ function FullTrip() {
     window.scrollTo(0, 0);
   }, []);
 
+  useEffect(() => {
+    const footer = document.querySelector('footer');
+    const handleScroll = () => {
+      if (footer) {
+        const footerRect = footer.getBoundingClientRect();
+        const buttonHeight = 25;
+
+        if (footerRect.top < window.innerHeight) {
+          setButtonBottom(window.innerHeight - footerRect.top + buttonHeight);
+        } else {
+          setButtonBottom(0);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleScroll);
+    };
+  }, []);
+
   return (
-    <>
+    <div className="relative pb-20"> {/* Add padding to the bottom to prevent content from being hidden behind the button */}
       {tripDetails &&
         <TripPreview
           avatar={tripDetails.avatar}
@@ -63,8 +90,17 @@ function FullTrip() {
         )
       }
       <CommentsContainer />
-    </>
+      <div style={{ bottom: `${buttonBottom}px` }} className="fixed left-0 w-full flex justify-center py-4">
+        <button 
+          onClick={() => navigate(-1)} 
+          className="px-4 py-2 bg-white text-black border border-primary rounded"
+        >
+          Back to Trips List
+        </button>
+      </div>
+    </div>
   );
 };
 
 export default FullTrip;
+
